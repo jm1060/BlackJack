401736e2ac4275033047147bc0e1ddc95e4d3973
James <jm1060@rit.edu>
2024-10-07 21:42:40 -0400

    Getting started on BlackJack

---
+++ blackjava.java
@@ -0,0 +1,29 @@
+public class blackjava
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Welcome a game of BlackJack.");
+        System.out.println("The rules are you take two cards you can decide to hit or stop.");
+        System.out.println("However, if you get over 21, you are out then your opponent wins");
+        System.out.println("Let's play.");
+        System.out.println(blackjackgame(0, 0, "", ""));
+    }
+    private static String blackjackgame(int playertotal, int opponenttotal, String playerresponse, String opponentresponse)
+    {
+        if(playerresponse.equals("stop") && opponentresponse.equals("stop"))
+        {
+
+        }
+        else
+        {
+            
+        }
+
+        return "";
+    }
+    private void addplayerscores(int playertotal, int input)
+    {
+
+    }
+
+}
