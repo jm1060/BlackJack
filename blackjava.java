523c9ee29587ddc8a8c547880612e093a36d0bd9
James <jm1060@rit.edu>
2024-10-08 11:59:29 -0400

    Got BlackJack to function properly, with oppponent being more competitive trying to think like real player would, taking a risk or not.

--- blackjava.java
+++ blackjava.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -9,26 +10,35 @@ public class blackjava
         System.out.println("The rules are you take two cards you can decide to hit or stop.");
         System.out.println("However, if you get over 21, you are out then your opponent wins");
         System.out.println("Let's play.");
+        int playertotal = 0;
+        int opponenttotal = 0;
+        System.out.println(blackjackgame(playertotal, opponenttotal, "", ""));
         
-        // System.out.println(blackjackgame(0, 0, "", ""));
+
     }
     private static String blackjackgame(int playertotal, int opponenttotal, String playerresponse, String opponentresponse)
     {
         if(playerresponse.equals("stop") && opponentresponse.equals("stop"))
         {
-
+            return checkwin(playertotal, opponenttotal);
         }
         else
         {
-            List<String> list = List.of("A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K");
+           List<String> list = Arrays.asList("A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K");
             Random random = new Random();
             Scanner scanner = new Scanner(System.in);
             if(playertotal == 0 && opponenttotal == 0)
             {
-                playertotal+=addplayerscores(playertotal, list.get(random.nextInt(list.size())));
-                playertotal+=addplayerscores(playertotal, list.get(random.nextInt(list.size())));
-                opponenttotal += addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
-                opponenttotal += addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
+                int number1 = addplayerscores(list.get(random.nextInt(list.size())));
+                playertotal+=number1;
+                int number2 = addplayerscores(list.get(random.nextInt(list.size())));
+                playertotal+= number2;
+                System.out.println(playertotal);
+                int opnum1 = addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
+                opponenttotal += opnum1;
+                int opnum2 = addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
+                opponenttotal += opnum2;
+                System.out.println(opponenttotal);
             }
             else{
                 if(!(playerresponse.equals("stop")))
@@ -37,16 +47,25 @@ public class blackjava
                     playerresponse = scanner.nextLine();
                     if(playerresponse.equals("hit"))
                     {
-                        playertotal += addplayerscores(playertotal, list.get(random.nextInt(list.size())));
+                        playertotal += addplayerscores(list.get(random.nextInt(list.size())));
+                        System.out.println("Player total: "+playertotal);
+                        playerresponse = checkplayerscores(playertotal, playerresponse);
+                    }
+                    else{
+                        playertotal += 0;
+                        System.out.println("Player total: "+playertotal);
                     }
                 }
                 else{
                     playertotal += 0;
+                    System.out.println("Player total: "+playertotal);
                 }
                 if(opponenttotal <= 10)
                 {
                     opponentresponse = "hit";
-                    opponenttotal = addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
+                    opponenttotal += addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
+                    System.out.println("Opponent total: "+opponenttotal);
+                    
                 }
                 else
                 {
@@ -54,10 +73,13 @@ public class blackjava
                     opponentresponse = choices.get(random.nextInt(choices.size()));
                     if(opponentresponse.equals("hit"))
                     {
-                        opponenttotal = addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
+                        opponenttotal += addopponentscores(opponenttotal, list.get(random.nextInt(list.size())));
+                        System.out.println("Opponent total: "+opponenttotal);
+                        opponentresponse = checkopponentscores(opponenttotal, opponentresponse);
                     }
                     else{
                         opponenttotal += 0;
+                        System.out.println("Opponent total: "+opponenttotal);
                     }
                 } 
             }
@@ -65,18 +87,20 @@ public class blackjava
             
         }
 
-        return "";
+        return blackjackgame(playertotal, opponenttotal, playerresponse, opponentresponse);
     }
-    private static int addplayerscores(int playertotal, String card)
+    private static int addplayerscores(String card)
     {
+        int number =0;
         Scanner scanner = new Scanner(System.in);
-        List<String>royals = List.of("J, Q, K");
+        List<String>royals = List.of("J", "Q", "K");
         if(royals.contains(card))
         {
-            playertotal += 10;
+            return 10;
         }
         else if(card.equals("A"))
         {
+            
             System.out.println("Would like A to be 1 or 11?");
             int acenumber = scanner.nextInt();
             while(acenumber != 1 && acenumber != 11)
@@ -86,26 +110,27 @@ public class blackjava
             }
             if(acenumber == 1)
             {
-                playertotal +=1;
+                number = acenumber;
             }
             else if(acenumber == 11)
             {
-                playertotal+=11;
+                number = acenumber;
             }
             
         }
         else{
-            playertotal += Integer.parseInt(card);
+            number = Integer.parseInt(card);
         }
-        return playertotal;
+        return number;
     }
+        
     private static int addopponentscores(int opponenttotal, String card)
     {
-        
-        List<String>royals = List.of("J, Q, K");
+        int opnum = 0;
+        List<String>royals = List.of("J", "Q", "K");
         if(royals.contains(card))
         {
-            opponenttotal += 10;
+            return 10;
         }
         else if(card.equals("A"))
         {
@@ -113,18 +138,18 @@ public class blackjava
             if(opponenttotal <= 10)
             {
                 int acenumber = 11;
-                opponenttotal += acenumber; 
+                opnum = acenumber;
             }
             else{
                 int acenumber = 1;
-                opponenttotal += acenumber;
+                opnum = acenumber;
             }
             
         }
         else{
-            opponenttotal += Integer.parseInt(card);
+            opnum = Integer.parseInt(card);
         }
-        return opponenttotal;
+        return opnum;
     }
     private static String checkplayerscores(int playertotal,String playerresponse)
     {
@@ -150,6 +175,34 @@ public class blackjava
             opponentresponse = "stop";
         }
         return opponentresponse;
+    }
+    private static String checkwin(int playertotal, int opponenttotal)
+    {
+        String finalstatement = "";
+        if(playertotal == opponenttotal)
+        {
+            finalstatement = "We have a tie. Player: "+ playertotal+ " Opponent: "+opponenttotal;
+        }
+        else if(playertotal > opponenttotal)
+        {
+            if(playertotal > 21)
+            {
+                finalstatement = "Busted! You lost! Player: "+ playertotal+ " Opponent: "+opponenttotal;
+            }
+            else{
+                finalstatement = "Congratulations! You've won! Player: "+ playertotal+ " Opponent: "+opponenttotal;
+            }
+        }
+        else{
+            if(opponenttotal > 21)
+            {
+                finalstatement = "Opponent is busted! You've won! Player: "+ playertotal+ " Opponent: "+opponenttotal;
+            }
+            else{
+                finalstatement = "Opponent has won! You've lost! Player: "+ playertotal+ " Opponent: "+opponenttotal;
+            }
+        }
+        return finalstatement;
     }
 
 }
