4c7a4d04d30261cf686d026c93a15ddc83f904fc
James <jm1060@rit.edu>
2024-10-08 08:53:15 -0400

    Finished adding scores for player

--- blackjava.java
+++ blackjava.java
@@ -1,6 +1,6 @@
 import java.util.List;
 import java.util.Random;
-
+import java.util.Scanner;
 public class blackjava
 {
     public static void main(String[]args)
@@ -24,15 +24,43 @@ public class blackjava
             Random random = new Random();
             if(playertotal == 0 && opponenttotal == 0)
             {
-                list.get(random.nextInt(list.size()));
+                playertotal=addplayerscores(playertotal, list.get(random.nextInt(list.size())));
             }
         }
 
         return "";
     }
-    private void addplayerscores(int playertotal, String card)
+    private static int addplayerscores(int playertotal, String card)
     {
+        Scanner scanner = new Scanner(System.in);
         List<String>royals = List.of("J, Q, K");
+        if(royals.contains(card))
+        {
+            playertotal += 10;
+        }
+        else if(card.equals("A"))
+        {
+            System.out.println("Would like A to be 1 or 11?");
+            int acenumber = scanner.nextInt();
+            while(acenumber != 1 && acenumber != 11)
+            {
+                System.out.println("Invalid number try again:");
+                acenumber = scanner.nextInt();
+            }
+            if(acenumber == 1)
+            {
+                playertotal +=1;
+            }
+            else if(acenumber == 11)
+            {
+                playertotal+=11;
+            }
+            
+        }
+        else{
+            playertotal += Integer.parseInt(card);
+        }
+        return playertotal;
     }
 
 }
